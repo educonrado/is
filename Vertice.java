09814a5204138d648c938ae62d26cd4dcf2eb9a6
Deb <excz.test@gmail.com>
2017-07-24 16:56:38 -0500

    Create Vertice.java

---
+++ Vertice.java
@@ -0,0 +1,28 @@
+package org.live.is;
+
+public class Vertice {
+	
+	private int horizontal;
+	private int vertical;
+	private int giro;
+	
+	public int getHorizontal() {
+		return horizontal;
+	}
+	public void setHorizontal(int derecha) {
+		this.horizontal = derecha;
+	}
+	public int getVertical() {
+		return vertical;
+	}
+	public void setVertical(int izquierda) {
+		this.vertical = izquierda;
+	}
+	public int getGiro() {
+		return giro;
+	}
+	public void setGiro(int giro) {
+		this.giro = giro;
+	}
+
+}
